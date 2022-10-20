5debf9dac96fcc881b6fecea36154e876af2019b
Vepr984 <serj90klishin@gmail.com>
2022-10-20 21:27:17 +0300

    chore: add content

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("Hello World!");
+import 'reseter.css/src/scss/reseter.scss';
+import './index.scss';
+
+
+const body = document.querySelector('body');
+
+// console.dir(body);
+// body.append('Hello World');
+// const button = document.createElement('button');
+// body.append(button);
+// button.append('Click me');
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+numbers.forEach((number) => {
+    const button = document.createElement('button');
+    button.append(number);
+    button.classList.add('btn');
+    body.append(button);
+
+    button.addEventListener('click', () => {
+        console.log(number);
+    })
+
+    button.addEventListener('mouseenter', () => {
+        button.classList.add('btn-hover');
+        console.log(number);
+    })
+});
+
