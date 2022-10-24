05a194129af6d5692a3a5f5d22086a652d686128
Vepr984 <serj90klishin@gmail.com>
2022-10-24 23:29:22 +0300

    chore: add func

--- src/index.js
+++ src/index.js
@@ -4,26 +4,77 @@ import './index.scss';
 
 const body = document.querySelector('body');
 
-// console.dir(body);
-// body.append('Hello World');
-// const button = document.createElement('button');
-// body.append(button);
-// button.append('Click me');
+// create new array with 16 numbers ordered randomly
+const numbers = Array.from({ length: 16 }, (v, k) => k + 1).sort(() => Math.random() - 0.5);
+// create div element for each number
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-numbers.forEach((number) => {
-    const button = document.createElement('button');
-    button.append(number);
-    button.classList.add('btn');
-    body.append(button);
+const fieldElement = document.createElement('div');
+fieldElement.classList.add('field');
 
-    button.addEventListener('click', () => {
-        console.log(number);
-    })
-
-    button.addEventListener('mouseenter', () => {
-        button.classList.add('btn-hover');
-        console.log(number);
-    })
+const cellsElements = numbers.map(number => {
+    const cellElement = document.createElement('div');
+    cellElement.classList.add('cell');
+    if (number === 16) {
+        const emptyItemElement = document.createElement('div');
+        emptyItemElement.classList.add('item', 'empty');
+        emptyItemElement.dataset.number = number;
+        cellElement.appendChild(emptyItemElement);
+    } else {
+        const itemElement = document.createElement('div');
+        itemElement.classList.add('item');
+        itemElement.textContent = number;
+        itemElement.dataset.number = number;
+        cellElement.appendChild(itemElement);
+        itemElement.addEventListener('click', () => {
+            const emptyItem = fieldElement.querySelector('.empty');
+            const emptyItemNumber = emptyItem.dataset.number;
+            const itemNumber = itemElement.dataset.number;
+            if (Math.abs(emptyItemNumber - itemNumber) === 16 ) {
+                emptyItem.textContent = itemNumber;
+                emptyItem.dataset.number = itemNumber;
+                itemElement.textContent = '';
+                itemElement.dataset.number = '';
+            }
+        })
+    }
+    return cellElement;
 });
 
+// when you click on a tile next to an empty cell, the tile moves to the empty cell
+
+
+
+fieldElement.append(...cellsElements);
+body.append(fieldElement);
+
+
+
+
+// const cellsElements = numbers.map(number => {
+//     const cellElement = document.createElement('div');
+//     cellElement.classList.add('cell');
+//     if (number === 16) {
+//         const emptyItemElement = document.createElement('div');
+//         emptyItemElement.classList.add('item', 'empty');
+//         emptyItemElement.dataset.number = number;
+//         cellElement.appendChild(emptyItemElement);
+//     } else {
+//         const itemElement = document.createElement('div');
+//         itemElement.classList.add('item');
+//         itemElement.textContent = number;
+//         itemElement.dataset.number = number;
+//         cellElement.appendChild(itemElement);
+//         itemElement.addEventListener('click', () => {
+//             const emptyItem = fieldElement.querySelector('.empty');
+//             const emptyItemNumber = emptyItem.dataset.number;
+//             const itemNumber = itemElement.dataset.number;
+//             if (Math.abs(emptyItemNumber - itemNumber) === 16 || Math.abs(emptyItemNumber - itemNumber) === 4) {
+//                 emptyItem.textContent = itemNumber;
+//                 emptyItem.dataset.number = itemNumber;
+//                 itemElement.textContent = '';
+//                 itemElement.dataset.number = '';
+//             }
+//         })
+//     }
+//     return cellElement;
+// });
